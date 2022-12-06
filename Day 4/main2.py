06724d6daafb54907527ea3a9dcbeb4b31f5d0bb
Oliver Malkin <oliver.malkin@gmail.com>
2022-12-06 19:47:54 +0000

    Day 5 pt 1

--- Day 4/main2.py
+++ Day 4/main2.py
@@ -7,11 +7,8 @@ for i in range(len(pairs)):
     pair = pairs[i].split(',') # current pair
     for j in range(0,2):
         pair[j] = pair[j].split('-')
-    print(pair)
 
-    if int(pair[0][0]) <= int(pair[1][0]) and int(pair[0][1]) >= int(pair[1][1]) or int(pair[0][0]) >= int(pair[1][0]) and int(pair[0][1]) <= int(pair[1][1]):
-        overlapping += 1
-    elif int(pair[0][1]) >= int(pair[1][0]) and int(pair[0][0]) <= int(pair[1][1]) or int(pair[0][0]) <= int(pair[1][1]) and int(pair[1][0]) <= int(pair[0][1]):
+    if int(pair[0][1]) >= int(pair[1][0]) and int(pair[0][0]) <= int(pair[1][1]) or int(pair[0][0]) <= int(pair[1][1]) and int(pair[1][0]) <= int(pair[0][1]):
         overlapping += 1
 
 print(overlapping)
