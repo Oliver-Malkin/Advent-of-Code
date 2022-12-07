563311132f307b5c678578f228d9ad8aecfd156e
Oliver Malkin <oliver.malkin@gmail.com>
2022-12-07 00:19:10 +0000

    Day 5 pt 2

---
+++ Day 5/main2.py
@@ -0,0 +1,39 @@
+with open("Day 5/input.txt", "r") as file:
+    stacks_raw = file.read().split('\n')
+
+with open("Day 5/input2.txt", "r") as file:
+    moves = file.read().split('\n')
+
+for i in range(len(moves)):
+    moves[i] = moves[i].replace('move', '').replace('to', '').replace('from', '').split()
+
+stacks = {}
+cols = int((len(stacks_raw[0])+1)/4) # Gets number of columns 
+for x in range(cols):
+    stacks[x+1] = []
+
+for i in stacks: # colums
+    for j in range(len(stacks_raw)): # rows
+        if stacks_raw[j][(4*(i)-3)] != ' ':
+            stacks[i].append(stacks_raw[j][(4*(i)-3)]) # put them into columns rather than rows
+
+for i in stacks:
+    stacks[i] = stacks[i][::-1] # Reverse all the lists
+
+for i in range(len(moves)): # Now make the moves [number, from, to]
+    temp = []
+    for j in range(0, int(moves[i][0])): # How many to move
+        From = int(moves[i][1])
+        To = int(moves[i][2])
+        temp.append(stacks[From][-1])
+        stacks[From].pop(-1)
+
+    temp = temp[::-1]
+    for k in range(len(temp)):
+        stacks[To].append(temp[k])
+
+top_boxes = []
+for i in stacks:
+    top_boxes.append(stacks[i][-1])
+
+print(''.join(top_boxes))
